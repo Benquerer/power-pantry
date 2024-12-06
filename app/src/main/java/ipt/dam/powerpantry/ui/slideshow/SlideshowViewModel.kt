d7854c0a766b17f0b1664683f54fe77cb3373e81
Benquerer <andrebenquerer@gmail.com>
2024-12-06 17:53:19 +0000

    Empty project
    Created empty "Navigation Drawer Views Activity" project

---
+++ app/src/main/java/ipt/dam/powerpantry/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package ipt.dam.powerpantry.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
