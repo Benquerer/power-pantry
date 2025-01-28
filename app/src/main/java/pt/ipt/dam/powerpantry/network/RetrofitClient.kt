0ad5a760178b8e04d3320df62b830b0583fe6674
Patinho88 <diogocardeira88@gmail.com>
2025-01-28 17:05:09 +0000

    update agp

--- app/src/main/java/pt/ipt/dam/powerpantry/network/RetrofitClient.kt
+++ app/src/main/java/pt/ipt/dam/powerpantry/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.sheety.co/powerPantryApi/folha1"
+    private const val BASE_URL = "https://api.sheety.co/fbdeb11d5ff39b33516d3da032f62ddf/powerPantryApi/folha1"
 
     val instance: SheetyApiService by lazy {
         Retrofit.Builder()
