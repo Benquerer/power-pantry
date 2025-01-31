0bb993e3bd343bafe7a983a7fa05ea1acb88092d
Benquerer <andrebenquerer@gmail.com>
2025-01-31 16:55:54 +0000

    Registration Logic
    - added full registration logic
    - needs only to actually post the user

--- app/src/main/java/pt/ipt/dam/powerpantry/data/User.kt
+++ app/src/main/java/pt/ipt/dam/powerpantry/data/User.kt
@@ -2,7 +2,6 @@ package pt.ipt.dam.powerpantry.data
 
 //single row (user)
 data class User(
-    val id: Int,
     val userName: String,
     val email: String,
     val password: String,
--- app/src/main/java/pt/ipt/dam/powerpantry/ui/login/LoginFragment.kt
+++ app/src/main/java/pt/ipt/dam/powerpantry/ui/login/LoginFragment.kt
@@ -3,6 +3,7 @@ package pt.ipt.dam.powerpantry.ui.login
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +13,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.coroutines.delay
+import org.mindrot.jbcrypt.BCrypt
 import pt.ipt.dam.powerpantry.MainActivity
 import pt.ipt.dam.powerpantry.R
 import pt.ipt.dam.powerpantry.api.DataRepository
+import pt.ipt.dam.powerpantry.data.User
+import retrofit2.Callback
 
 class LoginFragment : Fragment() {
 
@@ -75,29 +80,33 @@ class LoginFragment : Fragment() {
                 }else{
                     //check pass match
                     if(!passwd.matches(passwdConfirm.toRegex())){
-                        Toast.makeText(requireContext(), "Password confirmation failed: Make sure the passwords are the same", Toast.LENGTH_LONG).show()
+                        Toast.makeText(requireContext(), "The passwords don't match", Toast.LENGTH_SHORT).show()
                     }else{
+                        //disable button
+                        btnRegister.isEnabled = false
+                        btnRegister.text = "Registering..."
                         //check if user already exists
                         checkUsernameAvailable(username) { isAvailable ->
                             if (isAvailable) {
-                                registerUser("Registrando user")
-                                //clear inputs
-                                sheetView.findViewById<EditText>(R.id.etRegisterUsername).setText("")
-                                sheetView.findViewById<EditText>(R.id.etRegisterEmail).setText("")
-                                sheetView.findViewById<EditText>(R.id.etRegisterPassword).setText("")
-                                sheetView.findViewById<EditText>(R.id.etRegisterConfirmPassword).setText("")
+                                //register
+                                registerUser(username,email,passwd) {result ->
+                                    if(result){
+                                        registerSheet.dismiss()
+                                        Toast.makeText(requireContext(), "Registration completed", Toast.LENGTH_LONG).show()
+                                    }else{
+                                        Toast.makeText(requireContext(), "ERROR IN CREATION API SIDE", Toast.LENGTH_LONG).show()
+                                    }
 
-                                //////// CONTINUAR AQUI AMANHA
-
-
+                                }
                             } else {
-                                Toast.makeText(requireContext(), "Username is already taken! Please pick another one", Toast.LENGTH_LONG).show()
+                                Toast.makeText(requireContext(), "Username is already taken! Please pick another one", Toast.LENGTH_SHORT).show()
+                                //restore button
+                                btnRegister.isEnabled = true
+                                btnRegister.text = "Register Now"
                             }
                         }
                     }
                 }
-
-
             }
 
             //show sheet
@@ -110,18 +119,24 @@ class LoginFragment : Fragment() {
         DataRepository.checkUserExists(
             username,
             onResult = { exists ->
-                onResult(!exists) // If user exists (`true`), then username is NOT available (`false`)
+                onResult(!exists)
             },
             onError = { message ->
                 Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
-                onResult(false) // Assume unavailable in case of error
+                onResult(false)
             }
         )
     }
 
 
-    fun registerUser(message : String){
-        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+    fun registerUser(username : String, password : String, email: String, onResult: (Boolean) -> Unit){
+        onResult(true)
+        //hash password
+        val hashedPasswd = BCrypt.hashpw(password,BCrypt.gensalt(5))
+        //create user
+        val newUser = User(userName = username, email = email, password = hashedPasswd)
+        //post user
+
     }
 
 }
